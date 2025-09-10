48d517c54df08d697b440e06c1fb67d09fed7bc8
Keith Yatco <keith.yatco@snsofttech.ph>
2025-09-10 17:06:51 +0800

    Installed npm i @grpc/reflection

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ReflectionService } from '@grpc/reflection';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
