f8ab6cba054467b8829f364bb344166e9d7b8155
Keith Yatco <keith.yatco@snsofttech.ph>
2025-09-11 09:22:34 +0800

    Initial implementation of employee grpc

---
+++ src/employee/employee.controller.ts
@@ -0,0 +1,21 @@
+import { Controller } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { Metadata } from '@grpc/grpc-js';
+import { ServerUnaryCall } from '@grpc/grpc-js';
+import { GetEmployeeRequest, GetEmployeeResponse } from './proto/employee';
+
+@Controller('employee')
+export class EmployeeController {
+  @GrpcMethod('EmployeeService', 'GetEmployee')
+  findOne(
+    data: GetEmployeeRequest,
+    metadata: Metadata,
+    call: ServerUnaryCall<any, any>,
+  ): GetEmployeeResponse {
+    const items = [
+      { _id: 1, name: 'John' },
+      { _id: 2, name: 'Doe' },
+    ];
+    return items.find(({ _id }) => _id === data._id);
+  }
+}
