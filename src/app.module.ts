a872cf1b609e9a28f36071be97f0c1acbe49c7d7
Keith Yatco <keith.yatco@snsofttech.ph>
2025-09-12 14:32:53 +0800

    Initial implementation of gRPC

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeeModule } from './employee/employee.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
+//app.module.ts
 @Module({
-  imports: [EmployeeModule],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'PLAYGROUND_PACKAGE',
+        transport: Transport.GRPC,
+        options: {
+          package: 'employee',
+          protoPath: join(process.cwd(), 'proto/employee.proto'),
+        },
+      },
+    ]),
+    EmployeeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
+import { ReflectionService } from '@grpc/reflection';
 
+//main.ts
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.GRPC,
-    options: {
-      package: ['employee'],
-      protoPath: [join(__dirname, 'proto/employee.proto')],
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: ['employee'],
+        protoPath: [join(process.cwd(), 'proto/employee.proto')],
+        url: `0.0.0.0:${process.env.PORT ?? 9000}`,
+        onLoadPackageDefinition(pkg, server) {
+          new ReflectionService(pkg).addToServer(server);
+        },
+      },
     },
-  });
+    { inheritAppConfig: true },
+  );
 
   await app.listen(process.env.PORT ?? 9000);
 }
